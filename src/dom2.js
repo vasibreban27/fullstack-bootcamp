914fa13373aef862caeab43cf8dd423847dbc9e1
Vasi Breban <vbreban09@gmail.com>
2026-04-16 19:12:01 +0300

    not working now

--- src/dom2.js
+++ src/dom2.js
@@ -20,28 +20,47 @@ for (const todo of prevData) {
 addTodoForm.addEventListener('submit', handleAddTodo);
 
 function handleAddTodo(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  const formData = new FormData(e.target);
-  const title = formData.get('title');
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
 
-  const newTodo = { title, completed: false };
+    const newTodo = {
+        id: crypto.randomUUID(),
+        title,
+        completed: false
+    };
 
-  prevData.push(newTodo);
+    prevData.push(newTodo);
+    localStorage.setItem(storageKey, JSON.stringify(prevData));
 
-  localStorage.setItem(storageKey, JSON.stringify(prevData));
-  // const todoElement = createTodoElement(formData.get('title'));
- 
-  const todoElement = createTodoFromTemplate(newTodo, todoTemplate);
+    const todoElement = createTodoFromTemplate(newTodo, todoTemplate);
+    todoList.append(todoElement);
 
-  todoList.append(todoElement);
+    e.target.reset();
 }
 
 function createTodoFromTemplate(todo, todoTemplate) {
-  const todoElem = todoTemplate.cloneNode(true);
-  todoElem.querySelector('label').append(todo.title);
+    const todoElement = todoTemplate.cloneNode(true);
+    todoElement.querySelector('label').append(todo.title);
 
-  return todoElem;
+    const input = todoElement.querySelector('input');
+    input.dataset.todoId = todo.id;
+    input.checked = todo.completed;
+    input.addEventListener('change', handleCompleteTodo);
+
+    return todoElement;
+}
+
+function handleCompleteTodo(e) {
+    const checkbox = e.target;
+    const todoId = checkbox.dataset.todoId;
+
+    const todoToChange = prevData.find(todoObj => todoObj.id === todoId);
+    if (!todoToChange) return;
+
+    todoToChange.completed = checkbox.checked;
+    localStorage.setItem(storageKey, JSON.stringify(prevData));
 }
 
 function createTodoElement(title) {
@@ -51,6 +70,7 @@ function createTodoElement(title) {
 
   input.type = 'checkbox';
   input.name = 'completed';
+
   label.append(input, title);
   li.append(label);
 
