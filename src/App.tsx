37dd2d5222d469a43780985b75f94cdaaa423979
Vasi Breban <vbreban09@gmail.com>
2026-04-23 21:08:44 +0300

    start react learning

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import {Counter} from "./features/Counter/Counter"
+
+function App(){
+    return <h1>Counter</h1>
+}
+
+export {App}
---
+++ src/features/Counter/Counter.tsx
@@ -0,0 +1,23 @@
+export function Counter(){
+
+    let cnt = 0;
+
+    function handleClick(){
+        cnt--;
+    }
+
+
+
+    return (
+        <>
+            <h1>Counter</h1>
+            <output>{cnt}</output>
+            <p>
+                <button onClick={handleClick}>+</button>
+                <button>-</button>
+            </p>
+        </>
+
+    )
+}
+
