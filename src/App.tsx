7ce1f5eac4c86b2f9f5adb18620a86b7bfb43e9a
Vasi Breban <vbreban09@gmail.com>
2026-05-05 19:47:55 +0300

    my version of nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Counter } from "./features/Counter/Counter";
 import { Todos } from "./features/Todos/Todos";
 import { Weather } from "./features/Weather/Weather";
+import {Route, Routes} from "react-router";
+import './App.css';
+
 
 export function App() {
   return (
-  <>
-    <Todos />
-    <Weather />
-    <Counter />
-    <Counter initialCount={3} diff={5} />
-  </>
+  <Routes>
+      <Route path="todos" element={<Todos />} />
+      <Route path="weather" element={<Weather />} />
+      <Route
+          path="counter"
+          element={
+      <>
+        <Counter initialCount={10} diff={2} />
+        <Counter/>
+      </>
+      }
+    />
+  </Routes>
   );
 }
